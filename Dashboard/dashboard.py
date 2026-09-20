ada63e1189775b35ebaa8b811434acf0697d542a
Nove Lorenta <143886855+Novelorenta09@users.noreply.github.com>
2026-09-20 23:25:53 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -15,7 +15,8 @@ st.set_page_config(
 # set stye seaborn
 sns.set_theme(style="whitegrid")
 
-dashboard_df = pd.read_csv("all_table_df.zip")
+# dashboard_df = pd.read_csv("all_table_df.zip")
+dashboard_df = pd.read_csv("Dashboard/all_table_df.zip")
 dashboard_df['order_purchase_timestamp'] = pd.to_datetime(
     dashboard_df['order_purchase_timestamp']
 )
